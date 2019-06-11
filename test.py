b530212f9375615d0a50683f9c34967f7cff29ae
Wodka <domi.erdnuss2@gmx.de>
2019-06-11 14:29:56 -0600

    Fix activation bug

--- test.py
+++ test.py
@@ -5,18 +5,6 @@ from model import *
 
 tf.enable_eager_execution()
 
-X = np.random.randn(1000, 25, 8)
-A = np.abs(np.random.randn(1000, 25, 25))
-A /= A.sum(1, keepdims=True)
-M = np.ones((1000, 25, 25))
-y = (np.random.randn(1000) < 0).astype(np.int)
-
-model = GraphConvolutionalNetwork(8)
-model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-model.fit([X, A, M], y)
-
-exit()
-
 class LossLoggingCalback(tf.keras.callbacks.Callback):
     """ Callback for logging the losses at the end of each epoch. """
 
@@ -40,8 +28,9 @@ class LossLoggingCalback(tf.keras.callbacks.Callback):
         
         print(f'Baseline accuracy {baseline_accuracy}')
 
-data = HD5Dataset('../data/data_dragon2.hd5', features=['ChargeFirstPulse', 'TimeFirstPulse', 'ChargeLastPulse', 'TimeLastPulse',
-    'IntegratedCharge', 'TimeStdDeviation', 'VertexX', 'VertexY', 'VertexZ'])
+"""
+data = HD5Dataset('../data/data_dragon3.hd5', features=['ChargeFirstPulse', 'TimeFirstPulse', 'ChargeLastPulse', 'TimeLastPulse',
+    'IntegratedCharge', 'ChargeMaxPulse', 'TimeMaxPulse', 'TimeVariance', 'VertexX', 'VertexY', 'VertexZ'])
 batch_size = 64
 def data_generator():
     for (X, C, M), y in data.get_batches(batch_size=batch_size):
@@ -54,7 +43,7 @@ model = tf.keras.Sequential([
     tf.keras.layers.Dense(32, activation='relu'),
     tf.keras.layers.Dense(1, activation='sigmoid')
 ])
-
+"""
 """
 X = np.random.randn(1000, 100, 10, 3)
 A = np.ones([1000, 100, 10, 10])
@@ -75,11 +64,24 @@ model = tf.keras.Sequential(layers=[
 
 model = RecurrentGraphConvolutionalNetwork(3, units_graph_convolutions=[64, 64], units_fully_connected=[16, 1], units_lstm=[16, 16])
 """
+
+n = 5000
+
+X = np.random.randn(n, 25, 5)
+A = np.random.randn(n, 25, 25)
+M = np.zeros([n, 25, 25])
+M[:, :15, :15] = 1
+y = (np.random.randn(n) > 0).astype(np.int)
+X[y > 0] = np.abs(X[y > 0])
+X[y <= 0] = -np.abs(X[y <= 0])
+model = GraphConvolutionalNetwork(5, use_batchnorm=False)
+
 optimizer = tf.keras.optimizers.Adam(lr=1e-4)
 loss = 'binary_crossentropy'
 model.compile(optimizer=optimizer, 
             loss=loss,
             metrics=['accuracy'])
+"""
 model.fit_generator(
         data_generator(), 
         steps_per_epoch = int(np.ceil(data.size(dataset='train') / batch_size)),
@@ -87,5 +89,6 @@ model.fit_generator(
         callbacks = [LossLoggingCalback()],
         class_weight = data.get_class_weights()
         )
-#model.fit(x=[X, A, M], y=y, epochs=20)
+"""
+model.fit(x=[X, A, M], y=y, epochs=20)
 #model.fit(x=X, y=y, epochs=20, batch_size=32)
