Add moving mean and variance to GCN BatchnormLayers and fix training vs testing issues

model/gcnn.py
@@ -5,12 +5,12 @@ from .graph import *
 from .util import *
 tf.enable_eager_execution()
 
-class GraphConvolutionalNetwork(keras.Model):
+class GraphConvolutionalNetwork(tf.keras.Model):
     """ Model for a Graph Convolutional Network with dense graph structure. The graph is obtained using
     a Gaussian kernel on the pairwise node distances. """
 
     def __init__(self, num_input_features, units_graph_convolutions = [64, 64], units_fully_connected = [32, 1], 
-        units_graph_features=None, dropout_rate=0.5, use_batchnorm=True, build_distances=False):
+        units_graph_features=None, dropout_rate=0.5, use_batchnorm=True, build_distances=False, **kwargs):
         """ Creates a GCNN model. 
 
         Parameters:
@@ -33,7 +33,7 @@ class GraphConvolutionalNetwork(keras.Model):
         build_distances : bool
             If the network is input with coordinates and has to build the pairwise distances itself.
         """
-        super().__init__()
+        super().__init__(**kwargs)
         self.is_binary_classifier = (units_graph_convolutions + units_fully_connected)[-1] == 1
         self.adjacency_layer = GaussianAdjacencyMatrix(build_distances=build_distances)
         self.graph_convolutions, self.fully_connecteds = [], []
@@ -47,7 +47,7 @@ class GraphConvolutionalNetwork(keras.Model):
                     hidden_dimension, 
                     dropout_rate = None if is_last_layer else dropout_rate,
                     use_activation = not is_last_layer,
-                    use_batchnorm = False #use_batchnorm and not is_last_layer,
+                    use_batchnorm = use_batchnorm and not is_last_layer,
                 )
             )
         
@@ -81,7 +81,8 @@ class GraphConvolutionalNetwork(keras.Model):
             self.graph_feature_layers = None
 
 
-    def call(self, inputs):
+    def call(self, inputs, training=True):
+        print(training)
         # Graph convolutions
         if self.graph_feature_layers is None:
             x, coordinates, masks = inputs
@@ -89,7 +90,7 @@ class GraphConvolutionalNetwork(keras.Model):
             x, graph_features, coordinates, masks = inputs
         A = self.adjacency_layer([coordinates, masks])
         for layer in self.graph_convolutions:
-            x = layer([x, A, masks])
+            x = layer([x, A, masks], training=training)
         # Average pooling of the node embeddings
         x = padded_vertex_mean(x, masks)
 

model/graph.py
@@ -85,27 +85,35 @@ class GraphConvolution(keras.layers.Layer):
         if self.dropout_rate:
             self.dropout = keras.layers.Dropout(rate=self.dropout_rate)
 
-    def call(self, inputs):
+    def call(self, inputs, training=None):
         x, A, masks = inputs
         hidden = self.dense(x)
         a = tf.matmul(A, hidden)
         #a = tf.concat([a, x], axis=-1)
         if self.use_batchnorm:
-            a = self.bn([a, masks])
+            a = self.bn([a, masks], training=training)
         if self.use_activation:
             a = self.activation(a)
             #x = tf.concat([x, activated], axis=-1)
         #a = tf.concat([a, x], axis=-1)
         if self.dropout_rate:
-            a = self.dropout(a)
+            a = self.dropout(a, training=training)
         return a
 
     
 class BatchNormalization(keras.layers.Layer):
     """ Layer that applies feature and batch normalization accounting for padded zeros. """
 
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
+    def __init__(self, *args, momentum=0.99, **kwargs):
+        """ Batchnorm layer that accounts for padded vertices.
+        
+        Parameters:
+        -----------
+        momentum : float
+            The momentum for the moving mean and variance.
+        """
+        super().__init__(*args, **kwargs)
+        self.momentum = momentum
 
     def build(self, input_shape):
         shape_X, _ = input_shape
@@ -114,14 +122,26 @@ class BatchNormalization(keras.layers.Layer):
             weight_shape[idx] = 1
         self.beta = self.add_weight('beta', shape = weight_shape)
         self.gamma = self.add_weight('gamma', shape = weight_shape)
+        self.moving_mean = self.add_weight('moving_mean', shape=weight_shape, trainable=False)
+        self.moving_variance = self.add_weight('moving_variance', shape=weight_shape, trainable=False)
 
-    def call(self, inputs):
+    def call(self, inputs, training=None):
         X, masks = inputs # X is of shape [num_samples, num_vertices, num_features]
-        # Normalize over batch and vertices
-        vertex_mean = tf.expand_dims(padded_vertex_mean(X, masks), -2)
-        batch_mean = tf.reduce_mean(vertex_mean, axis=0, keepdims=True)
-        X_centered = X - batch_mean
-        X_var = tf.expand_dims(padded_vertex_mean(X_centered ** 2, masks), -2)
-        X_var = tf.reduce_mean(X_var, axis=0, keepdims=True)
-        X_normalized = X_centered / (tf.sqrt(X_var) + 1e-20)
+        #X = tf.Print(X, [X], 'X')
+        if training: # This is super weird, that during fit
+            # Normalize over batch and vertices
+            vertex_mean = tf.expand_dims(padded_vertex_mean(X, masks), -2)
+            batch_mean = tf.reduce_mean(vertex_mean, axis=0, keepdims=True)
+            X_centered = X - batch_mean
+            batch_variance = tf.expand_dims(padded_vertex_mean(X_centered ** 2, masks), -2)
+            batch_variance = tf.reduce_mean(batch_variance, axis=0, keepdims=True)
+            X_normalized = X_centered / (tf.sqrt(batch_variance) + 1e-20)
+            # Update the moving mean and variance
+            self.moving_mean = self.momentum * self.moving_mean + (1 - self.momentum) * batch_mean
+            self.moving_variance = self.momentum * self.moving_variance + (1 - self.momentum) * batch_variance
+        else:
+            # Use the moving mean and variance to scale the batch
+            #self.moving_mean = tf.Print(self.moving_mean, [self.moving_mean], 'mv')
+            X_normalized = X - self.moving_mean / (tf.sqrt(self.moving_variance) + 1e-20)
+
         return self.gamma * X_normalized + self.beta

test.py
@@ -2,6 +2,8 @@ import numpy as np
 import tensorflow as tf
 from dataset import HD5Dataset
 from model import *
+from tensorflow import keras
+K = keras.backend
 
 tf.enable_eager_execution()
 
@@ -64,8 +66,51 @@ model = tf.keras.Sequential(layers=[
 
 model = RecurrentGraphConvolutionalNetwork(3, units_graph_convolutions=[64, 64], units_fully_connected=[16, 1], units_lstm=[16, 16])
 """
+"""
+class MyModel(tf.keras.Model):
 
-n = 10000
+    def __init__(self):
+        super().__init__()
+        self.fc = tf.keras.layers.Dense(1, activation='sigmoid')
+
+    def call(self, x, training=None):
+        if training:
+            tf.print('Asdf', training)
+            return self.fc(x)
+        tf.print('Not train', training)
+        x = tf.Print(x, [x], 'tr')
+        return self.fc(x)
+
+class MyLayer(tf.keras.layers.Layer):
+
+    def build(self, *args, **kwargs):
+        self.fc = tf.keras.layers.Dense(1, activation='sigmoid')
+    @tf.function
+    def call(self, x, training=None):
+        #if not training:
+        #    x = tf.Print(x, [x], 'tr')
+        return self.fc(x)
+
+
+X = np.random.randn(100, 100)
+y = (np.random.randn(100) > 0).astype(np.int)
+X[y < 0] = -np.abs(X[y < 0])
+
+model = MyModel()
+optimizer = tf.keras.optimizers.Adam(lr=1e-4)
+loss = 'binary_crossentropy'
+model.compile(optimizer=optimizer, 
+            loss=loss,
+            metrics=['accuracy'])
+
+
+model.fit(X, y, validation_split=0.1)
+
+model.predict(X[:10])
+exit(0)
+"""
+
+n = 50
 
 X = np.random.randn(n, 25, 5)
 F = np.random.randn(n, 10)
@@ -77,7 +122,7 @@ X[y > 0] = np.abs(X[y > 0])
 X[y <= 0] = -np.abs(X[y <= 0])
 F[y > 0] = np.abs(F[y > 0])
 F[y < 0] = -np.abs(F[y < 0])
-model = GraphConvolutionalNetwork(5, units_graph_features=[64, 64], use_batchnorm=False)
+model = GraphConvolutionalNetwork(5, units_graph_features=[64, 64], use_batchnorm=True)
 
 optimizer = tf.keras.optimizers.Adam(lr=1e-4)
 loss = 'binary_crossentropy'
@@ -93,5 +138,38 @@ model.fit_generator(
         class_weight = data.get_class_weights()
         )
 """
-model.fit(x=[X, F, A, M], y=y, epochs=20, validation_split=0.1)
+model.fit(x=[X, F, A, M], y=y, epochs=3, validation_split=0.1)
+
+print('Learning Phase', tf.keras.backend.learning_phase())
+
+# Check if the model provides consistent outputs along different batch and vertex sizes
+X = np.random.randn(n, 25, 5)
+F = np.random.randn(n, 10)
+A = np.random.randn(n, 25, 25)
+M = np.zeros([n, 25, 25])
+M[:, :15, :15] = 1
+y = (np.random.randn(n) > 0).astype(np.int)
+X[y > 0] = np.abs(X[y > 0])
+X[y <= 0] = -np.abs(X[y <= 0])
+F[y > 0] = np.abs(F[y > 0])
+F[y < 0] = -np.abs(F[y < 0])
+
+Xb = X[:16]
+Fb = F[:16]
+Ab = A[:16]
+Mb = M[:16]
+
+pred1 = model.predict_on_batch([Xb, Fb, Ab, Mb])
+
+Xb = X[:32]
+Fb = F[:32]
+Ab = A[:32]
+Mb = M[:32]
+
+
+pred2 = model.predict_on_batch([Xb, Fb, Ab, Mb])
+
+
+
+
 #model.fit(x=X, y=y, epochs=20, batch_size=32)